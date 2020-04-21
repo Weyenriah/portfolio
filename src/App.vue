c180640794f40085fc2740ad4f767c650a1f4bf3
Weyenriah <weyenriah@gmail.com>
2020-04-21 17:18:30 +0200

    add comma dangle

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
@@ -56,5 +56,5 @@ export default {
   data: () => ({
     //
   }),
-};
+}
 </script>
