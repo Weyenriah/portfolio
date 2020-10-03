83abdd7e6cc8349e59ddc09120c773f4deac84d7
Weyenriah <weyenriah@gmail.com>
2020-10-03 13:01:18 +0200

    add filter for projects

--- src/components/TheProjects.vue
+++ src/components/TheProjects.vue
@@ -6,13 +6,30 @@
       <v-icon class="greenify">fa-circle</v-icon>
       <v-icon class="greyify">fa-circle</v-icon>
     </h2>
-    <div class="projects-grid">
+    <v-chip-group
+      multiple
+      v-model="selectedTags"
+    >
+      <v-chip
+        active-class="chip-active"
+        :value="tag"
+        v-for="tag in availableTags"
+        :key="tag"
+      >
+        {{ tag }}
+      </v-chip>
+    </v-chip-group>
+    <transition-group
+      name="filtered-projects"
+      tag="div"
+      class="projects-grid"
+    >
       <v-card
         dark
         tile
         :to="project.to"
-        v-for="(project, i) in p"
-        :key="i"
+        v-for="(project) in p"
+        :key="project.id"
       >
         <v-img
           class="align-end project-image"
@@ -24,18 +41,18 @@
               {{ project.title }}
             </v-card-title>
             <div class="d-flex">
-              <v-card-subtitle
+              <v-chip
                 v-for="tag in project.tags"
                 :key="tag"
                 class="project-type"
               >
                 {{ tag }}
-              </v-card-subtitle>
+              </v-chip>
             </div>
           </div>
         </v-img>
       </v-card>
-    </div>
+    </transition-group>
   </v-container>
 </template>
 
@@ -60,12 +77,37 @@ export default {
         })
       })
     },
+    availableTags () {
+      const s = new Set()
+
+      this.projects.forEach((project) => {
+        project.tags.forEach((tag) => {
+          s.add(tag)
+        })
+      })
+      return s
+    },
     ...mapState(['projects']),
   },
 }
 </script>
 
 <style lang="scss" scoped>
+  .chip-active {
+    background-color: #059809;
+    color: white !important;
+  }
+  .filtered-projects-enter, .filtered-projects-leave-to {
+    height: 3px;
+    opacity: 0;
+  }
+  .filtered-projects-enter-to, .filtered-projects-leave {
+    height: 100%;
+    opacity: 1;
+  }
+  .filtered-projects-enter-active, .filtered-projects-leave-active {
+    transition: all 0.19s ease;
+  }
   .projects-grid {
     display: grid;
     grid-template-columns: calc(33% - 10px) calc(33% - 10px) calc(33% - 10px);
@@ -80,11 +122,8 @@ export default {
           padding: 10px 0 0 15px;
         }
         .project-type {
-          width: 33%;
-          padding: 0;
           text-align: center;
           background-color: #059809;
-          border-radius: 3px;
           margin-left: 15px;
           transition: all .5s;
         }
--- src/store/projects.js
+++ src/store/projects.js
@@ -1,12 +1,13 @@
 export default [
   {
+    id: 0,
     title: 'Örebro Katthem',
     tags: ['website'],
     to: '#',
     img: require('../assets/orebro-katthem.jpg'),
   },
-  { title: 'The Climate Report', tags: ['game'], to: '#', img: require('../assets/the-climate-report.jpg') },
-  { title: 'The Penguin Company', tags: ['logotype'], to: '#', img: require('../assets/the-penguin-company.jpg') },
-  { title: 'F-W', tags: ['logotype'], to: '#', img: require('../assets/fw-logo.jpg') },
-  { title: 'Mobilatrygghetslarm.se', tags: ['website', 'wordpress'], to: '#', img: require('../assets/mobilatrygghetslarm.jpg') },
+  { id: 1, title: 'The Climate Report', tags: ['game'], to: '#', img: require('../assets/the-climate-report.jpg') },
+  { id: 2, title: 'The Penguin Company', tags: ['logotype'], to: '#', img: require('../assets/the-penguin-company.jpg') },
+  { id: 3, title: 'F-W', tags: ['logotype'], to: '#', img: require('../assets/fw-logo.jpg') },
+  { id: 4, title: 'Mobilatrygghetslarm.se', tags: ['website', 'wordpress'], to: '#', img: require('../assets/mobilatrygghetslarm.jpg') },
 ]
