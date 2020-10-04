bc9865b71f5c1fe808d66b9862b6f2cc0579fa5d
Weyenriah <weyenriah@gmail.com>
2020-10-04 21:43:35 +0200

    style changes to showcase

--- src/components/TheShowcase.vue
+++ src/components/TheShowcase.vue
@@ -5,7 +5,7 @@
     cycle
     :interval="8000"
     progress
-    progress-color="#d00202"
+    progress-color="#0edcdc"
   >
     <v-carousel-item
       v-for="(project) in p"
@@ -38,14 +38,12 @@
                 </v-chip-group>
               </div>
               <h2>
-                <span class="thin">{{ project.thinTitle }}</span>{{ project.boldTitle }}
+                <span class="thinify">{{ project.thinTitle }}</span>{{ project.boldTitle }}
               </h2>
               <p> {{ project.excerpt }} </p>
               <router-link :to="'project/' + project.slug">
-                <v-icon class="redify small-dots">fa-circle</v-icon>
-                <v-icon class="whiteify small-dots">fa-circle</v-icon>
-                <v-icon class="whiteify small-dots">fa-circle</v-icon>
                 Go to project
+                <v-icon>fa-chevron-right</v-icon>
               </router-link>
             </section>
           </div>
@@ -74,11 +72,9 @@ export default {
     display: flex;
     justify-content: flex-end;
     .carousel-container {
-      // border-left: 10px solid $redify;
       background-color: rgba(0,0,0,0.5);
-      padding: 10px 50px 10px 40px;
+      padding: 0 40px;
       width: 50%;
-      transform: skewX(-5deg) translateX(30px);
       transition: width .5s;
       height: 100%;
       justify-content: center;
@@ -91,28 +87,23 @@ export default {
             margin-left: 5px;
           }
           .carousel-type {
-            background-color: $redify;
-
-          }
-        }
-
-        h2 {
-          .thin {
-            font-weight: normal;
+            background-color: $accent;
+            height: 24px;
           }
         }
         a {
+          text-align: right;
           text-decoration: none;
           color: white;
           transition: all .5s;
           &:hover {
-            // font-weight: bold;
-            .whiteify {
-              color: $redify !important;
+            i {
+              margin-left: 12px;
             }
           }
           i {
-            font-size: 18px;
+            font-size: 15px;
+            margin-left: 7px;
           }
         }
       }
@@ -121,18 +112,9 @@ export default {
   @media only screen and (max-width: 725px) {
     .carousel-flex {
       .carousel-container {
-        width: 90%;
-        padding-left: 20px;
+        width: 100%;
+        padding: 0 30px;
       }
     }
-  }
-
-  /* To get smoother transition
-   * From: https://github.com/vuetifyjs/vuetify/issues/10809
-   */
-  .v-carousel .v-window-item {
-    position: absolute;
-    top: 0;
-    width: 100%;
   }
 </style>
