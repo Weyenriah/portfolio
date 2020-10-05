19109b7d391dbbf45c25416e3e62ca59a50c6150
Weyenriah <weyenriah@gmail.com>
2020-10-05 20:45:09 +0200

    seperate the project into more components

--- src/components/TheProject.vue
+++ src/components/TheProject.vue
@@ -5,65 +5,29 @@
       height="100px"
       class="header-img"
     />
-    <v-container
-      class="project-inner-container"
-    >
+    <v-container class="project-inner-container">
       <v-sheet class="text-based-content">
         <section class="d-flex align-center project-title">
           <h2>
             <span class="thinify">{{ project.thinTitle }}</span> {{ project.boldTitle }}
           </h2>
         </section>
-        <section class="d-flex project-chips">
-          <v-chip
-            v-for="tag in project.tags"
-            :key="tag"
-            class="project-chip mt-1"
-          >
-            {{ tag }}
-          </v-chip>
-        </section>
+        <TheProjectTagGroups/>
         <section class="mt-4">
-          <div class="d-flex mb-2 roles">
-            <p class="mb-0 mr-9"><b>My role/s</b></p>
-            <div class="d-flex flex-wrap">
-              <span
-                v-for="(roles, i) in project.roles"
-                :key="i"
-                class="d-flex"
-              >
-                <p
-                  v-if="i !== 0"
-                  class="mr-3 mb-0"
-                >
-                  <b>|</b>
-                </p>
-                <p class="mb-0 mr-3">{{ roles }}</p>
-              </span>
-            </div>
-          </div>
+          <TheProjectRoles/>
           <v-divider/>
-          <div class="d-flex mt-2 mb-4 techniques">
-            <p class="mr-9 mb-2"><b>Techniques</b></p>
-            <div class="d-flex flex-wrap">
-              <span
-                v-for="(tech, i) in project.techniques"
-                :key="i"
-                class="d-flex"
-                >
-                <p
-                  v-if="i !== 0"
-                  class="mr-3 mb-0"
-                >
-                  <b>|</b>
-                </p>
-                <p class="mb-0 mr-3">{{ tech }}</p>
-              </span>
-            </div>
-          </div>
+          <TheProjectTechniques/>
         </section>
         <section>
           {{ project.excerpt }}
+        </section>
+        <section>
+          <v-sheet
+            class="process-bubble"
+            width="50%"
+          >
+            <p>Text</p>
+          </v-sheet>
         </section>
         <section class="mt-4 go-to-project">
           <v-btn
@@ -77,27 +41,26 @@
           </v-btn>
         </section>
       </v-sheet>
-      <v-img
-        :src="require('../assets/images/computer.jpg')"
-        class="project-img"
-        v-if="project.focus === 'web'"
-      >
-        <v-img
-          :src="project.compImg"
-          class="nested-img"
-          width="75.7%"
-        >
-        </v-img>
-      </v-img>
+      <TheProjectImage/>
     </v-container>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import TheProjectRoles from './TheProjectRoles'
+import TheProjectTechniques from './TheProjectTechniques'
+import TheProjectTagGroups from './TheProjectTagGroups'
+import TheProjectImage from './TheProjectImage'
 
 export default {
   name: 'TheProject',
+  components: {
+    TheProjectImage,
+    TheProjectTagGroups,
+    TheProjectTechniques,
+    TheProjectRoles,
+  },
   computed: {
     project () {
       return this.projects.find((p) => p.slug === this.$route.params.slug)
@@ -133,26 +96,8 @@ export default {
   .project-inner-container {
     display: grid;
     grid-template-columns: 50% 50%;
-    .project-img {
-      .nested-img {
-        border-radius: 7px;
-        position: relative;
-        left: 12%;
-        top: 10.5%;
-      }
-    }
     .text-based-content {
       grid-column: 1;
-      .project-chips {
-        > :not(:first-child) {
-          margin-left: 5px;
-        }
-        .project-chip {
-          color: white;
-          background-color: $accent;
-          height: 24px;
-        }
-      }
       .go-to-project {
         text-align: right;
       }
@@ -162,13 +107,6 @@ export default {
     .project-inner-container {
       display: grid;
       grid-template-columns: 100%;
-      .techniques, .roles {
-        flex-direction: column;
-      }
-      .project-img {
-        grid-column: 1 !important;
-        grid-row: auto !important;
-      }
     }
   }
   @media only screen and (max-width: 450px) {
---
+++ src/components/TheProjectImage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-img
+    :src="require('../assets/images/computer.jpg')"
+    class="project-img"
+    v-if="project.focus === 'web'"
+  >
+    <v-img
+      :src="project.compImg"
+      class="nested-img"
+      width="75.7%"
+    >
+    </v-img>
+  </v-img>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'TheProjectImage',
+  computed: {
+    project () {
+      return this.projects.find((p) => p.slug === this.$route.params.slug)
+    },
+    ...mapState(['projects']),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .project-img {
+    .nested-img {
+      border-radius: 7px;
+      position: relative;
+      left: 12%;
+      top: 10.5%;
+    }
+  }
+  @media only screen and (max-width: 1050px) {
+    .project-img {
+      grid-column: 1 !important;
+      grid-row: auto !important;
+    }
+  }
+</style>
